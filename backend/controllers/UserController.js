98de1fcce63a3e785a80bac5f8f637d086328819
Aditya Bheke <adityabheke34@gmail.com>
2025-10-11 15:34:10 +0530

    feat: update user controller methods to handle async operations and return user data

--- backend/controllers/UserController.js
+++ backend/controllers/UserController.js
@@ -1,24 +1,30 @@
+import userService from "../services/UserService.js";
 
 // Register a user
-export const registerUser = (req, res, next) => {
+export const registerUser = async (req, res, next) => {
     try {
-        res.status(201).json({ message: 'User registered successfully' });
+        const userData = req.body;
+        const result = await userService.registerUser(userData);
+        res.status(201).json({ message: 'User registered successfully', user: result.user });
     } catch (error) {
         next(error);
     }
 };
 // Login a user
-export const loginUser = (req, res, next) => {
+export const loginUser = async (req, res, next) => {
     try {
-        res.status(200).json({ message: 'User logged in successfully' });
+        const { email, password } = req.body;
+        const result = await userService.loginUser({email, password});
+        res.status(200).json({ message: 'User logged in successfully', user: result.user, token: result.token });
     } catch (error) {
         next(error);
     }
 };
 // Get all users
-export const getAllUsers = (req, res, next) => {
+export const getAllUsers = async (req, res, next) => {
     try {
-        res.status(200).json({ message: 'Fetched all users successfully' });
+        const result = await userService.getAllUsers();
+        res.status(200).json({ message: 'Fetched all users successfully!', users: result });
     } catch (error) {
         next(error);
     }
